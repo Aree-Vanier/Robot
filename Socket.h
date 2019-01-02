9d842daa32d374f109ffabb34bb5a35c4af12a01
aree-vanier <gregory@ajayinkingston.com>
2019-01-01 23:58:09 -0500

    Attempted to use inheritance

--- Socket.h
+++ Socket.h
@@ -3,13 +3,14 @@
 #include <WebSocketsServer.h>
 
 
-class Socket{
+class Socket: private WebSocketsServer {
   public:
+    Socket();
     void init();
     void loop();
     bool enabled;
   private:
-  void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
+    void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
 };
 
 #endif
